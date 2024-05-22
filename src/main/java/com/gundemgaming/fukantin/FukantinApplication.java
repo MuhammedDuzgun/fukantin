5cc3fb4e69632df2a479dacad5638c409b394f0f
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-22 03:01:19 +0300

    Initial commit

---
+++ src/main/java/com/gundemgaming/fukantin/FukantinApplication.java
@@ -0,0 +1,13 @@
+package com.gundemgaming.fukantin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FukantinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FukantinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gundemgaming/fukantin/FukantinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gundemgaming.fukantin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FukantinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

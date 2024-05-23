b6e2f992b43aeaef650bc2ef32429c76822dc8f1
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-23 03:51:36 +0300

    Modify UserDto

--- src/main/java/com/gundemgaming/fukantin/dto/UserDto.java
+++ src/main/java/com/gundemgaming/fukantin/dto/UserDto.java
@@ -2,17 +2,11 @@ package com.gundemgaming.fukantin.dto;
 
 import com.gundemgaming.fukantin.model.Post;
 import com.gundemgaming.fukantin.model.Reply;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 public class UserDto {
     private Long id;
     private String username;

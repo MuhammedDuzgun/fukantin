54642330fab8c404fb2844dbde1ae608424578e4
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-23 03:49:47 +0300

    Modify IUserService

--- src/main/java/com/gundemgaming/fukantin/service/IUserService.java
+++ src/main/java/com/gundemgaming/fukantin/service/IUserService.java
@@ -1,14 +1,14 @@
 package com.gundemgaming.fukantin.service;
 
 import com.gundemgaming.fukantin.dto.UserDto;
-import com.gundemgaming.fukantin.dto.UserRegisterDto;
+import com.gundemgaming.fukantin.dto.UserAuthDto;
 
 import java.util.List;
 
 public interface IUserService {
     List<UserDto> getAllUsers();
     UserDto getUser(Long userId);
-    UserRegisterDto createUser(UserRegisterDto userRegisterDto);
-    UserRegisterDto updateUser(UserRegisterDto userRegisterDto, Long userId);
+    UserAuthDto createUser(UserAuthDto userAuthDto);
+    UserAuthDto updateUser(UserAuthDto userAuthDto, Long userId);
     void deleteUser(Long userId);
 }

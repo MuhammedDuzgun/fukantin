5f49c9aaf2646dbf336c40e00fb14b2545cf0268
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-25 01:19:20 +0300

    Add custom methods to IUserRepository

--- src/main/java/com/gundemgaming/fukantin/repository/IUserRepository.java
+++ src/main/java/com/gundemgaming/fukantin/repository/IUserRepository.java
@@ -3,5 +3,12 @@ package com.gundemgaming.fukantin.repository;
 import com.gundemgaming.fukantin.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IUserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
 }

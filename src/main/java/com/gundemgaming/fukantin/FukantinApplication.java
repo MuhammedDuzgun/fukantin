2f7b2dadd7b167f668ca9899ff87c3b94829f8fd
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-25 01:17:20 +0300

    Add ModelMapper bean

--- src/main/java/com/gundemgaming/fukantin/FukantinApplication.java
+++ src/main/java/com/gundemgaming/fukantin/FukantinApplication.java
@@ -1,10 +1,17 @@
 package com.gundemgaming.fukantin;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FukantinApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FukantinApplication.class, args);

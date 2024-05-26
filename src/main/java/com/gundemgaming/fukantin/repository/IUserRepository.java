a4a4f8698fead443ba33188960c5bfe7ddc35d4c
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-26 03:32:56 +0300

    Modify IUserRepository

--- src/main/java/com/gundemgaming/fukantin/repository/IUserRepository.java
+++ src/main/java/com/gundemgaming/fukantin/repository/IUserRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 public interface IUserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
-
     Boolean existsByUsername(String username);
 
 }

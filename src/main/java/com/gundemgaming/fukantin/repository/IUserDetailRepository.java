a3eba5b71100c096655f031a84d409d16fb086d5
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-26 18:57:36 +0300

    Modify IUserDetailRepository

--- src/main/java/com/gundemgaming/fukantin/repository/IUserDetailRepository.java
+++ src/main/java/com/gundemgaming/fukantin/repository/IUserDetailRepository.java
@@ -3,5 +3,5 @@ package com.gundemgaming.fukantin.repository;
 import com.gundemgaming.fukantin.model.UserDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserDetail extends JpaRepository<UserDetail, Long> {
+public interface IUserDetailRepository extends JpaRepository<UserDetail, Long> {
 }

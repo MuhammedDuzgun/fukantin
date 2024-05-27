fa7a2eb58077205a9d7d4e24566f3af025a23caa
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-27 20:00:08 +0300

    Add custom method to IPostRepository

--- src/main/java/com/gundemgaming/fukantin/repository/IPostRepository.java
+++ src/main/java/com/gundemgaming/fukantin/repository/IPostRepository.java
@@ -3,5 +3,8 @@ package com.gundemgaming.fukantin.repository;
 import com.gundemgaming.fukantin.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IPostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByCategoryId(Long categoryId);
 }

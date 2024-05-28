fb4e0b51b47bcbacadbb743081577f98c4032d0a
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-28 03:01:01 +0300

    Modify IReplyRepository

--- src/main/java/com/gundemgaming/fukantin/repository/IReplyRepository.java
+++ src/main/java/com/gundemgaming/fukantin/repository/IReplyRepository.java
@@ -3,5 +3,10 @@ package com.gundemgaming.fukantin.repository;
 import com.gundemgaming.fukantin.model.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IReplyRepository extends JpaRepository<Reply, Long> {
+
+    List<Reply> findByPostId(Long postId);
+
 }

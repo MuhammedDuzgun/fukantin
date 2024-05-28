1b2bdc37e1cb63e9ab94721f22ed2be29811c772
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-28 03:00:44 +0300

    Modify IReplyService

--- src/main/java/com/gundemgaming/fukantin/service/IReplyService.java
+++ src/main/java/com/gundemgaming/fukantin/service/IReplyService.java
@@ -9,7 +9,7 @@ public interface IReplyService {
 
     List<ReplyDto> getAllRepliesByPostId(Long postId);
     ReplyDto getReply(Long postId, Long replyId);
-    ReplyDto createReply(ReplyDto replyDto, Long postId);
+    ReplyDto createReply(ReplyDto replyDto, Long postId, Long userId);
     ReplyDto updateReply(ReplyDto replyDto, Long postId, Long replyId);
     void deleteReply(Long postId, Long replyId);
 

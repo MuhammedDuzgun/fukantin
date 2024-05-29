cb33fb4661e3989b5a2634858db7e40548dfc042
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-05-29 17:40:38 +0300

    Modify: IUserDetailService

--- src/main/java/com/gundemgaming/fukantin/service/IUserDetailService.java
+++ src/main/java/com/gundemgaming/fukantin/service/IUserDetailService.java
@@ -3,6 +3,6 @@ package com.gundemgaming.fukantin.service;
 import com.gundemgaming.fukantin.dto.UserDetailDto;
 
 public interface IUserDetailService {
-    UserDetailDto getUserDetail(Long userId, Long userDetailId);
-    UserDetailDto updateUserDetail(UserDetailDto userDetailDto, Long userId, Long userDetailId);
+    UserDetailDto getUserDetail(Long userId);
+    UserDetailDto updateUserDetail(UserDetailDto userDetailDto, Long userId);
 }

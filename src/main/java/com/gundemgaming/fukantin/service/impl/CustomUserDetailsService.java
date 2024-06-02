20591f9253953c5cb1117e25da9e1f6b0d2c4549
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-06-02 04:01:09 +0300

    Create CustomUserDetailsService

---
+++ src/main/java/com/gundemgaming/fukantin/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.gundemgaming.fukantin.service.impl;
+
+import com.gundemgaming.fukantin.model.User;
+import com.gundemgaming.fukantin.repository.IUserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final IUserRepository userRepository;
+
+    public CustomUserDetailsService(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found with username : " + username));
+
+        Set<GrantedAuthority> authorities = user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRole())).collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
+
+
+
+}

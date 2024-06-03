aafb8dea223fae1e826031eb4d7985427fdb15fc
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-06-03 04:22:53 +0300

    Modify : SecurityConfig

--- src/main/java/com/gundemgaming/fukantin/config/SecurityConfig.java
+++ src/main/java/com/gundemgaming/fukantin/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.gundemgaming.fukantin.config;
 
+import com.gundemgaming.fukantin.security.JwtAuthenticationEntryPoint;
+import com.gundemgaming.fukantin.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,15 +18,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
     private UserDetailsService userDetailsService;
+    private JwtAuthenticationEntryPoint authenticationEntryPoint;
+    private JwtAuthenticationFilter authenticationFilter;
 
-    public SecurityConfig(UserDetailsService userDetailsService) {
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          JwtAuthenticationEntryPoint authenticationEntryPoint,
+                          JwtAuthenticationFilter authenticationFilter) {
         this.userDetailsService = userDetailsService;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.authenticationFilter = authenticationFilter;
     }
 
     @Bean
@@ -41,11 +51,19 @@ public class SecurityConfig {
         httpSecurity.csrf(csrf-> csrf.disable())
                 .authorizeHttpRequests(authorize->
                         authorize
-                                .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .requestMatchers("api/auth/**").permitAll()
                                 .requestMatchers("/api/categories/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .requestMatchers( "/api/users/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+
+                ).exceptionHandling(exception-> exception
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                ).sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        httpSecurity.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }

e5ed2047c2c24ed1d307360def31f74cf0648d5a
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-06-03 04:15:03 +0300

    Modify User entity

--- src/main/java/com/gundemgaming/fukantin/model/User.java
+++ src/main/java/com/gundemgaming/fukantin/model/User.java
@@ -1,10 +1,7 @@
 package com.gundemgaming.fukantin.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 import java.util.Set;
